f283e5325f2ad3ceb8eb32776692960235926539
Gavin1217 <gavinr1@wloo.org>
2021-01-28 09:37:10 -0600

    Create ReyesName.py

---
+++ ReyesName.py
@@ -0,0 +1,28 @@
+from turtle import*
+canvas = Screen()
+canvas.setup(400,200)
+gavin = Turtle()
+gavin.shape("turtle")
+gavin.color("green")
+gavin.forward(30)
+gavin.right(90)
+gavin.forward(50)
+gavin.left(270)
+gavin.forward(60)
+gavin.right(90)
+gavin.forward(90)
+gavin.right(90)
+gavin.forward(60)
+gavin.left(180)
+gavin.forward(60)
+gavin.left(90)
+gavin.forward(90)
+gavin.left(90)
+gavin.forward(60)
+gavin.forward(25)
+gavin.circle(25)
+gavin.forward(20)
+gavin.
+
+
+
